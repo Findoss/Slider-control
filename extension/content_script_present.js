ac2db493fc1430586c4701ed9c203b7b8d99f165
Nikita Stroganov <nstroganov@adsterra.com>
2021-11-25 15:37:18 +0300

    декомпозтровал структуру расширения

--- extension/content_script_present.js
+++ extension/content_script_present.js
@@ -1,20 +1,81 @@
-(async () => {
-  async function PresentPage() {
-    console.log('PresentPage');
-    const store = await loadGoogleData();
-    await getKey(store);
+/**
+ * @format
+ */
 
-    console.log(store);
+const $buttonNext = document.querySelector(".punch-viewer-navbar-next");
+const $buttonPre = document.querySelector(".punch-viewer-navbar-prev");
+
+(async () => {
+  async function init() {
+    getGoogleData();
+    initEventListeners();
   }
 
+  // получаем и отправляем данные в вебворкер презентации
+  async function getGoogleData(params) {
+    const googleData = await loadGoogleData();
+    await sendGoogleData(googleData);
+  }
+
+  // вешаем обработчики на презентацию для синхронизации
+  async function initEventListeners(params) {
+    function debounce(f, t = 300) {
+      let timer;
+      return (...args) => {
+        if (!timer) {
+          f.apply(this, args);
+        }
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          timer = undefined;
+        }, t);
+      };
+    }
+
+    function onScroll(event) {
+      if (event.deltaY > 0) {
+        sendNextSlide(event);
+      } else {
+        sendPrevSlide(event);
+      }
+    }
+
+    $buttonNext.addEventListener("click", sendNextSlide);
+    $buttonPre.addEventListener("click", sendPrevSlide);
+
+    document.addEventListener("click", sendNextSlide);
+
+    document.addEventListener("keyup", (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          sendPrevSlide(event);
+          break;
+        case "ArrowRight":
+          sendNextSlide(event);
+          break;
+        case "ArrowUp":
+          sendPrevSlide(event);
+          break;
+        case "ArrowDown":
+          sendNextSlide(event);
+          break;
+      }
+    });
+
+    window.addEventListener("mousewheel", debounce(onScroll, 70), {
+      passive: false,
+    });
+  }
+
+  // загружаем данные презы из контекста страницы
   async function loadGoogleData() {
     return new Promise((res, rej) => {
-      const $script = document.createElement('script');
-      $script.src = chrome.runtime.getURL('execute.js');
+      const $script = document.createElement("script");
+      $script.src = chrome.runtime.getURL("execute.js");
 
       document.body.appendChild($script);
 
-      document.body.addEventListener('init-slide-control', e => {
+      document.body.addEventListener("init-slide-control", (e) => {
         const store = e.detail.googleDataView;
         res(store);
         $script.remove();
@@ -22,25 +83,47 @@
     });
   }
 
-  async function getKey(payload) {
-    const api = 'http://localhost:5010';
-    const data = JSON.stringify(payload);
-
-    console.log('getKey');
-    chrome.runtime.sendMessage(
-      {
-        contentScriptQuery: 'postFetch',
-        data: data,
-        url: `${api}/key`
-      }
-      // response => {
-      //   console.log(response);
-      //   if (response) {
-      //     console.log(response);
-      //   }
-      // }
-    );
+  async function sendGoogleData() {
+    //
   }
 
-  PresentPage();
+  async function sendNextSlide(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    chrome.runtime.sendMessage({
+      contentScriptQuery: "postFetch",
+      data: {
+        id: "next-slide",
+      },
+      url: `${api}/key`,
+    });
+    console.log("sendNextSlide");
+  }
+
+  async function sendPrevSlide(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log("sendPrevSlide");
+  }
+
+  async function getKey(payload) {
+    // const api = 'http://localhost:5010';
+    // const data = JSON.stringify(payload);
+    // console.log('getKey');
+    // chrome.runtime.sendMessage(
+    //   {
+    //     contentScriptQuery: 'postFetch',
+    //     data: data,
+    //     url: `${api}/key`
+    //   }
+    // response => {
+    //   console.log(response);
+    //   if (response) {
+    //     console.log(response);
+    //   }
+    // }
+    // );
+  }
+
+  init();
 })();
--- extension/service_worker.js
+++ extension/service_worker.js
@@ -1,33 +1,35 @@
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  if (request.contentScriptQuery == 'getFetch') {
-    const url = request.url;
-    // fetch(url)
-    //   .then(response => response.json())
-    //   .then(response => sendResponse(response))
-    //   .catch();
-    return true;
-  }
+importScripts('service-worker-utils.js')
 
-  if (request.contentScriptQuery == 'postFetch') {
-    try {
-      let raw = await fetch(request.url, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: request.data
-      });
-      const data = await raw.json();
+// chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+//   if (request.contentScriptQuery == 'getFetch') {
+//     const url = request.url;
+//     // fetch(url)
+//     //   .then(response => response.json())
+//     //   .then(response => sendResponse(response))
+//     //   .catch();
+//     return true;
+//   }
 
-      console.log(data);
-      sendResponse(data);
-    } catch (error) {
-      console.error(error);
-    }
+//   if (request.contentScriptQuery == 'postFetch') {
+//     try {
+//       let raw = await fetch(request.url, {
+//         method: 'POST',
+//         headers: {
+//           Accept: 'application/json',
+//           'Content-Type': 'application/json'
+//         },
+//         body: request.data
+//       });
+//       const data = await raw.json();
 
-    return true;
-  }
+//       console.log(data);
+//       sendResponse(data);
+//     } catch (error) {
+//       console.error(error);
+//     }
 
-  return true;
-});
+//     return true;
+//   }
+
+//   return true;
+// });
