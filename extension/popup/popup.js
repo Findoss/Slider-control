20c0c0ea8ae199920f0b1786b0eef7f460d43f88
Nikita Stroganov <nstroganov@adsterra.com>
2021-12-02 16:43:12 +0300

    add events start (get key)

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,18 +1,47 @@
-const $key = document.querySelector('.section_key__number');
-const $buttonNewKey = document.querySelector('.button_new_key');
+/**
+ * @format
+ */
+
+const $blockPrompt = document.querySelector(".prompt");
+const $blockControl = document.querySelector(".control");
+
+const $key = document.querySelector(".section_key__number");
+const $buttonNewKey = document.querySelector(".button_new_key");
 
 function init() {
-  $buttonNewKey.addEventListener('click', getNewKey);
-  getNewKey();
+  $buttonNewKey.addEventListener("click", start);
+  renderControl();
+}
+
+function renderPrompt() {
+  $blockPrompt.classList.remove("hide");
+  $blockControl.classList.add("hide");
+}
+
+function renderControl() {
+  $blockPrompt.classList.add("hide");
+  $blockControl.classList.remove("hide");
 }
 
 function updateKey(key) {
   $key.textContent = JSON.stringify(key);
 }
 
-function getNewKey() {
-  chrome.runtime.sendMessage({ action: 'get-key' }, key => {
-    // if (key) updateKey(key);
+function getKey() {
+  chrome.runtime.sendMessage({ action: "get-key" }, (key) => {
+    console.log(key);
+    updateKey(key);
+  });
+}
+
+function isPresent() {
+  console.log(location.host);
+  return location.host === "docs.google.com";
+}
+
+function start() {
+  chrome.runtime.sendMessage({ action: "start-presentation" }, (key) => {
+    console.log(key);
     updateKey(key);
   });
 }
