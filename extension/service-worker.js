0a9be7caf0a54b3171fc600284308b5a230323d6
Nikita Stroganov <nstroganov@adsterra.com>
2021-12-02 16:42:47 +0300

    add start and ypdate slide

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -2,50 +2,88 @@
  * @format
  */
 
-importScripts('service-worker-utils.js');
+importScripts("service-worker-utils.js");
+
+const NEXT_BUTTON_CODE = 39;
+const PREV_BUTTON_CODE = 37;
+
+function init() {
+  chrome.runtime.onMessage.addListener((req, info, cb) => {
+    if (req.action === "start-presentation") {
+      startPresentation().then((key) => {
+        cb(key);
+      });
+    }
+
+    if (req.action === "get-key") {
+      getKey().then(({ key }) => {
+        cb(key);
+      });
+    }
+
+    return true;
+  });
+
+  chrome.webNavigation.onHistoryStateUpdated.addListener(updateSlide);
+}
 
 function setKey(key) {
   chrome.storage.local.set({ key: key });
 }
 
 function getKey() {
-  return chrome.storage.local.get('key');
+  return chrome.storage.local.get("key");
 }
 
-function init() {
-  chrome.runtime.onMessage.addListener(e => {
-    setKey(startPresentation(e));
-  });
-
-  chrome.runtime.onMessage.addListener((req, info, cb) => {
-    if (req.action === 'get-key') {
-      const key = getKey();
-
-      if (key) {
-        cb(key);
-      } else {
-        cb(startPresentation());
-      }
-
-      return true;
-    }
-  });
-
-  chrome.webNavigation.onHistoryStateUpdated.addListener(updateSlide);
-}
-
-function updateSlide(e) {
+async function updateSlide(e) {
   const { url } = e;
 
-  const searchParams = new URLSearchParams(new URL(url).searchParams);
+  const id = new URL(url).pathname.split("/")[3];
+  const slide = new URL(url).searchParams.get("slide").substring(3);
+  const { key } = await getKey();
+
+  const slideData = {
+    id,
+    key,
+    slide,
+  };
+
+  fetch("http://localhost:5010/update", {
+    method: "POST",
+    body: JSON.stringify(slideData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
   // todo send server next slide
-  console.log(searchParams.get('slide'));
 }
 
-function startPresentation(data) {
-  console.log(data);
-  return Math.random();
+async function startPresentation() {
+  const presentationData = await new Promise((res, rej) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "get-data-presentation" },
+        (data) => {
+          res(data);
+        },
+      );
+    });
+  });
+
+  const raw = await fetch("http://localhost:5010/start", {
+    method: "POST",
+    body: JSON.stringify(presentationData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const { key } = await raw.json();
+  setKey(key);
+
+  return key;
 }
 
 init();
